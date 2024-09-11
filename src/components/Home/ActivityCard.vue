abda921d370147d976d67cf22987f20c869e25aa
Floor de Best <debestfloor@gmail.com>
2024-09-11 12:35:13 +0200

    fix activity card props

--- src/components/Home/ActivityCard.vue
+++ src/components/Home/ActivityCard.vue
@@ -23,22 +23,22 @@ const popUpStore = usePopUpStore()
       <p
         class="absolute bottom-[20px] flex text-center items-center text-core-lightest font-bold text-[21px]"
       >
-        {{ lang.languageFile[activity.name].toUpperCase() }}
+        {{ lang.languageFile[props.activity.name].toUpperCase() }}
       </p>
     </div>
     <div
       class="absolute top-0 left-0 flex flex-col z-0 items-center justify-center p-[15px] space-y-[20px] h-full bg-accent-1 overflow-y-auto"
     >
       <p class="text-[16px] lg:text-[21px] text-core-lightest font-bold">
-        {{ lang.languageFile[activity.name].toUpperCase() }}
+        {{ lang.languageFile[props.activity.name].toUpperCase() }}
       </p>
       <p class="text-[11px] lg:text-[14px] text-core-light pb-[10px]">
-        {{ lang.languageFile[activity.description] }}
+        {{ lang.languageFile[props.activity.description] }}
       </p>
       <CustomButton
         :title="lang.languageFile.bookAClassButton"
         :invertedColors="true"
-        @click="popUpStore.setClass(activity.name)"
+        @click="popUpStore.setClass(props.activity.name)"
       />
     </div>
   </div>
